98058d91eccc73ab706a2e69d82aac834389d38f
Alfredo Manuel <alfredomanuel127.0@gmail.com>
2023-05-17 16:29:22 +0100

    structure of project

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const dotenv = require("dotenv").config();
+const app =express();
+
+const port = process.env.PORT || 5000
+
+app.get("/api/contacts",(req,res)=>{
+  res.json({message:'hello express'})
+})
+
+app.listen(port,()=>{console.log(`SERVER RUNNING IN PORT: ${port}`)})
