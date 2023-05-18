24b243873eb2835a1bbaf060e4f786f24b5f9017
Alfredo Manuel <alfredomanuel127.0@gmail.com>
2023-05-18 20:52:17 +0100

    connection with mongoDB

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -1,45 +1,60 @@
-
+const asyncHandler = require("express-async-handler")
+const Contact = require('../models/contactModel')
 /**
  * @desc Get all Contacts
  * @route GET /api/contacts
  * @access public 
  */
-const getContacts = (req,res)=>{
-  res.status(200).json({message:'Get All Contacts'})
-}
+const getContacts = asyncHandler(async (req,res)=>{
+  const contacts = await Contact.find();
+  res.status(200).json(contacts)
+})
 /**
  * @desc Get Contacts
  * @route GET /api/contacts/id
  * @access public 
  */
- const getContact = (req,res)=>{
-  res.status(200).json({message:'Get All Contacts'})
-}
+ const getContact = asyncHandler(async (req,res)=>{
+ const contact = await Contact.find();
+ res.status(200).json(contact)
+});
 
 /**
  * @desc create new contacts
  * @route POST /api/contacts
  * @access public 
  */
- const createContact = (req,res)=>{
-  res.status(200).json({message:'Get All Contacts'})
-}
+ const createContact = asyncHandler(async (req,res)=>{
+  const { name , email , phone} = req.body;
+  if(!name || !email || !phone){
+    res.status(400)
+    throw new Error("All fields are mandatory")
+  }
+  const contact = await Contact.create({
+    name,
+    email,
+    phone,
+  })
+  res.status(200).json(contact);
+});
+
 /**
  * @desc update contacts
  * @route PUT /api/contacts/id
  * @access public 
  */
- const updateContact = (req,res)=>{
+ const updateContact = asyncHandler(async (req,res)=>{
   res.status(200).json({message:'Get All Contacts'})
-}
+});
 /**
  * @desc delete contacts
  * @route DELETE /api/contacts/id
  * @access public 
  */
- const deleteContact = (req,res)=>{
+ const deleteContact = asyncHandler(async (req,res)=>{
   res.status(200).json({message:'Get All Contacts'})
-}
+  });
+
 module.exports =  {
   getContacts,
   getContact,
--- server.js
+++ server.js
@@ -1,10 +1,15 @@
-const express = require('express')
+const express = require('express');
+const connectDB = require('./config/dbConnection');
+const errorHandler = require('./middleware/errorHandler');
 const dotenv = require("dotenv").config();
+
+connectDB();
 const app =express();
+const port = process.env.PORT || 5000;
 
-const port = process.env.PORT || 5000
-
+app.use(express.json())
 app.use("/api/contacts",require('./routes/contactRoute'));
+app.use(errorHandler)
 
 
 app.listen(port,()=>{console.log(`SERVER RUNNING IN PORT: ${port}`)})
