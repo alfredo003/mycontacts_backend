355578907e92e085efd7442738c05eb5d1118836
Alfredo Manuel <alfredomanuel127.0@gmail.com>
2023-05-19 23:44:29 +0100

    build user Model

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,10 +1,23 @@
 const asyncHandler = require("express-async-handler") 
+const User = require('./../models/userModel')
 /**
  * @desc Register a user
  * @route POST /api/users/regiter
  * @access public 
- */
+ */  
 const registerUser = asyncHandler(async (req,res)=>{
+  const  { username,email,password} = req.body;
+  if(!username || !email ||!password){
+    res.status(400);
+    throw new Error("All fields are mandatory")
+  }
+  const userAvailable = await User.findOne({email});
+  if(userAvailable){
+    res.status(400);
+    throw new Error("User already registered!")
+  }
+  
+
   res.json({message:"Regiter the user"});
 })
 
