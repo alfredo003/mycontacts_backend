1cb7c4beb28c323238f9b2823f38b748ac31ac12
Alfredo Manuel <alfredomanuel@pop-os.localdomain>
2023-05-20 20:32:03 +0100

    test

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,7 +2,7 @@ const asyncHandler = require("express-async-handler")
 const User = require('./../models/userModel')
 /**
  * @desc Register a user
- * @route POST /api/users/regiter
+ * @route POST /api/users/register
  * @access public 
  */  
 const registerUser = asyncHandler(async (req,res)=>{
