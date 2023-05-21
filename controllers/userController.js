72a2af92922e3cdf85a64c147efc615124d5fea7
alfredo003 <alfredomanuel127.0@gmail.com>
2023-05-21 14:04:21 +0100

    finish register user

--- controllers/userController.js
+++ controllers/userController.js
@@ -8,10 +8,14 @@ const User = require('./../models/userModel');
  */  
 const registerUser = asyncHandler(async (req,res)=>{
   const  { username,email,password} = req.body;
+
   if(!username || !email ||!password){
     res.status(400);
     throw new Error("All fields are mandatory")
   }
+
+ 
+
   const userAvailable = await User.findOne({email});
   if(userAvailable){
     res.status(400);
@@ -21,7 +25,19 @@ const registerUser = asyncHandler(async (req,res)=>{
   //Hash password
   const hashedPassword = await bcrypt.hash(password,10);
   console.log("Hashed Password :",hashedPassword);
+  const user = await User.create({
+    username,
+    email,
+    password: hashedPassword,
+  });
+  if(user){
+    res.status(201).json({_id:user.id,email:user.email})
+  }else{
+    res.status(400);
+    throw new Error("User data us not valid!")
+  }
 
+  console.log(`User created ${user}`)
   res.json({message:"Regiter the user"});
 })
 
--- models/userModel.js
+++ models/userModel.js
@@ -3,7 +3,7 @@ const userSchema =mongoose.Schema({
   username:{
     type:String,
     required:[true,"Please add the user name"]
-  },
+  }, 
   email:{
     type:String,
     required:[true,"Please add the user email address"],
